5d2db341507ef422fde83baa52d05c66eab2440c
Semiceri <semisnowy@gmail.com>
2016-03-14 22:44:45 -0700

    adding in more stuff for storage, will pull out to RDB

--- TwitterSampleStreamParser.py
+++ TwitterSampleStreamParser.py
@@ -1,38 +1,37 @@
-#Simple data stream of twitter used for pulling data into database
+# Simple data stream of twitter used for pulling data into database
 from twitter import TwitterStream, OAuth
-import sys
 import json
 
+
 def run(config):
-    #setting up auth from our config.json file
+    # setting up auth from our config.json file
     auth = OAuth(
         config['token'], config['token_secret'],
         config['consumer_key'], config['consumer_secret']
     )
 
-    #set limiter in config to cap the number of pulled tweets
+    # set limiter in config to cap the number of pulled tweets
     limiter = config['limiter']
     count = 0
 
-
-    #connect to default streaming endpoint
+    # connect to default streaming endpoint
     stream = TwitterStream(auth=auth)
     iterator = stream.statuses.sample()
 
-
-    #our stream loop
+    # our stream loop
     for tweet in iterator:
         if 'in_reply_to_status_id' in tweet:
             if limiter > 0:
                 count += 1
                 if count > limiter:
                     return
-            #found the key indicating it is a tweet, lets store it into an arbitrary file for now
-            print(tweet['text'])
+            # found the key indicating it is a tweet, lets store it into an arbitrary file for now
+            with open('data.json', 'a+') as data_file:
+                #append comma to json dump, will need to clean up last comma before processing. do it manually for now as it is one character.
+                data_file.write('%s,' % json.dumps(tweet))
 
-
-
-#open our json configuration file readonly
-with open ('config.json', 'r') as configuration_file:
+# open our json configuration file readonly
+with open('config.json', 'r') as configuration_file:
     config = json.load(configuration_file)
+
 run(config)
