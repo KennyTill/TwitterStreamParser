54475575070011e6a75c0a3a061d85c23ab471eb
Semiceri <semisnowy@gmail.com>
2016-03-16 12:22:43 -0700

    adding schema create and fixing single quote issue

--- setup.sql
+++ setup.sql
@@ -1,5 +1,10 @@
+CREATE DATABASE IF NOT EXISTS `TwitterItems`;
+
+USE `TwitterItems`;
+
+
 -- Basic table for now
-CREATE TABLE IF NOT EXISTS 'Tweets'(
+CREATE TABLE IF NOT EXISTS `Tweets`(
     id INTEGER PRIMARY KEY,
     tweet_text VARCHAR(255),
     lang VARCHAR(255),
